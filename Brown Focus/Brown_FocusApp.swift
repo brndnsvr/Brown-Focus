e0678ae34514e603ab59b9fd58c1f610e353f29c
Brandon Seaver <seaverb@icloud.com>
2025-03-21 13:58:26 -0400

    Initial Commit

---
+++ Brown Focus/Brown_FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brown_FocusApp.swift
+//  Brown Focus
+//
+//  Created by Brandon Seaver on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Brown_FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brown FocusTests/Brown_FocusTests.swift
@@ -0,0 +1,17 @@
+//
+//  Brown_FocusTests.swift
+//  Brown FocusTests
+//
+//  Created by Brandon Seaver on 3/21/25.
+//
+
+import Testing
+@testable import Brown_Focus
+
+struct Brown_FocusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
